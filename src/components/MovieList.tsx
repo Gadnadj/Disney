e23f2df2f1a87323d082e4390bf85f8a0f260be0
Gad Nadjar <gadnadjar@gmail.com>
2025-01-05 13:07:53 +0200

    add props

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {
+    genreId: number;
+}
+
+const MovieList = ({ genreId }: Props) => {
+    return (
+        <div>MovieList</div>
+    );
+};
+
+export default MovieList;
