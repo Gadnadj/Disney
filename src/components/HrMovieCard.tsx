2333e9ff8828d6a7bb977f967c2866b3e9cd6910
Gad Nadjar <gadnadjar@gmail.com>
2025-01-05 13:45:31 +0200

    add bigger card

---
+++ src/components/HrMovieCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {}
+
+const HrMovieCard = (props: Props) => {
+    return (
+        <div>HrMovieCard</div>
+    );
+};
+
+export default HrMovieCard;
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -3,14 +3,14 @@ import GlobalApi from '../Services/GlobalApi';
 import { Movie } from '../types';
 import MovieCard from './MovieCard';
 import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
-const screenWidth = window.innerWidth;
-
+import HrMovieCard from './HrMovieCard';
 
 type Props = {
     genreId: number;
+    index_: number;
 }
 
-const MovieList = ({ genreId }: Props) => {
+const MovieList = ({ genreId, index_ }: Props) => {
 
     const [movieList, setMovieList] = useState<Movie[]>([]);
 
@@ -26,29 +26,31 @@ const MovieList = ({ genreId }: Props) => {
     };
 
     const sliderRight = (element: HTMLDivElement) => {
-        element.scrollLeft += screenWidth - 110;
+        element.scrollLeft += 500;
     };
 
     const sliderLeft = (element: HTMLDivElement) => {
-        element.scrollLeft -= screenWidth - 110;
+        element.scrollLeft -= 500;
     };
 
     const elementRef = useRef<HTMLDivElement>(null);
 
 
     return (
-        <div>
+        <div className='relative'>
             <IoChevronBackOutline onClick={() => elementRef.current && sliderLeft(elementRef.current)}
-                className='text-[50px] text-white p-2 z-10 cursor-pointer hidden md:block absolute'
+                className='text-[50px] text-white p-2 z-10 cursor-pointer hidden md:block absolute mt-[138px]'
             />
 
-            <div className='flex overflow-x-auto gap-8  scrollbar-none pt-5 px-3 pb-5'>
+            <div ref={elementRef} className='flex overflow-x-auto gap-5 md:gap-8 scrollbar-none scroll-smooth pt-4 px-3 pb-4'>
                 {movieList.map((item, index) => (
-                    <MovieCard movie={item} key={index} />
+                    <>
+                        {index_ % 3 === 0 ? <HrMovieCard /> : <MovieCard movie={item} key={index} />}
+                    </>
                 ))}
             </div>
             <IoChevronForwardOutline onClick={() => elementRef.current && sliderRight(elementRef.current)}
-                className='text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 top-0 absolute right-0'
+                className='text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 top-0 absolute right-0 mt-[138px]'
             />
         </div>
     );
