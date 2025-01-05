d373af320b3dbc73f43d22992296391a102d0c04
Gad Nadjar <gadnadjar@gmail.com>
2025-01-05 12:48:32 +0200

    add shadow

--- src/components/ProductionHouse.tsx
+++ src/components/ProductionHouse.tsx
@@ -47,7 +47,16 @@ const ProductionHouse = (props: Props) => {
     ];
 
     return (
-        <div>ProductionHouse</div>
+        <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16 '>
+            {productionHouseList.map((item, index) => (
+                <div className='border-[2px] border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer shadow-xl shadow-black'>
+                    <video src={item.video} autoPlay loop playsInline
+                        className='absolute top-0 rounded-md z-0 opacity-0 hover:opacity-50' />
+                    <img src={item.image} alt="" key={index} className='w-full z-[1]' />
+
+                </div>
+            ))}
+        </div>
     );
 };
 
