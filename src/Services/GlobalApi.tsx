11c0232e0e4662f9d6a535ca34f8381e6f893bcb
Gad Nadjar <gadnadjar@gmail.com>
2025-01-05 14:01:33 +0200

    add env

--- src/Services/GlobalApi.tsx
+++ src/Services/GlobalApi.tsx
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-const moviesBaseUrl = 'https://api.themoviedb.org/3';
-const movieByGenreBaseURL = 'https://api.themoviedb.org/3/discover/movie?api_key=5ce5f1c74ea4723e6e0abc1d92478386';
-
-const api_key = '5ce5f1c74ea4723e6e0abc1d92478386';
-
+const moviesBaseUrl = import.meta.env.VITE_MOVIES_BASE_URL;
+const movieByGenreBaseURL = `${import.meta.env.VITE_MOVIE_BY_GENRE_BASE_URL}?api_key=${import.meta.env.VITE_API_KEY}`;
+const api_key = import.meta.env.VITE_API_KEY;
 
 const getTrendingVideos = axios.get(`${moviesBaseUrl}/trending/all/day?api_key=${api_key}`);
 const getMovieByGenreId = (id: number) => axios.get(movieByGenreBaseURL + '&with_genres=' + id);
-
-
 
 export default {
     getTrendingVideos,
