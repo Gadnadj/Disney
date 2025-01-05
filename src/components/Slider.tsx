ad64615cdfdc1e82a84965560d7b0b2aeb7c5522
Gad Nadjar <gadnadjar@gmail.com>
2025-01-05 11:56:08 +0200

    add picture movie

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,21 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import GlobalApi from '../Services/GlobalApi';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
+import { Movie } from '../types';
 
 type Props = {}
 
 const Slider = (props: Props) => {
+    const [movieList, setMovieList] = useState<Movie[]>([]);
     useEffect(() => {
         getTrendingMovies();
     }, []);
 
     const getTrendingMovies = () => {
         GlobalApi.getTrendingVideos.then(resp => {
-            console.log(resp);
+            console.log(resp.data.results);
+            setMovieList(resp.data.results);
 
         });
     };
     return (
-        <div>Slider</div>
+        <div className='flex'>
+            {movieList.map((item: Movie, index: number) => (
+                <img src={IMAGE_BASE_URL + item.backdrop_path}
+                    alt=""
+                    key={index}
+                    className='min-w-full h-[310px] object-cover' />
+            ))}
+        </div>
     );
 };
 
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,21 @@ export interface MenuItem {
     name: string;
     icon: React.ElementType;
 }
+
+export type Movie = {
+    adult: boolean;
+    backdrop_path: string;
+    first_air_date: string;
+    genre_ids: number[];
+    id: number;
+    media_type: string;
+    name: string;
+    origin_country: string[];
+    original_language: string;
+    original_name: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    vote_average: number;
+    vote_count: number;
+};
