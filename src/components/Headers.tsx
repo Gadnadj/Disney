03181305d2190eacdaf7d951586af7951ac89a6c
Gad Nadjar <gadnadjar@gmail.com>
2025-01-05 10:54:53 +0200

    add HiDotsVertical

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -2,6 +2,7 @@ import Logo from '../assets/Images/logo.png';
 import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv, HiPlus } from 'react-icons/hi2';
 import { MenuItem } from '../types';
 import HeaderItems from './HeaderItems';
+import { HiDotsVertical } from 'react-icons/hi';
 type Props = {}
 
 const Headers = () => {
@@ -38,9 +39,21 @@ const Headers = () => {
         <div className='flex flex-row gap-8 items-center justify-between p-5'>
             <div className='flex gap-8 items-center'>
                 <img src={Logo} alt="logo" className='w-[80px] md:w-[115px] object-cover' />
-                {menu.map((item: MenuItem) => (
-                    <HeaderItems name={item.name} Icon={item.icon} />
-                ))}
+                <div className='hidden md:flex md:gap-8'>
+                    {menu.map((item: MenuItem) => (
+                        <HeaderItems name={item.name} Icon={item.icon} />
+                    ))}
+                </div>
+
+                {/* Mobile Render */}
+                <div className='flex md:hidden gap-8'>
+                    {menu.map((item, index) => index < 3 && (
+                        <HeaderItems name={item.name} Icon={item.icon} />
+                    ))}
+                    <div className='flex gap-8'>
+                        <HeaderItems name={''} Icon={HiDotsVertical} />
+                    </div>
+                </div>
             </div>
             <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
                 className='w-[40px] rounded-full' />
